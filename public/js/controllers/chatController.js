1ea3a9790e0787362087cf909b6ba657dbae3bb6
Tiago Lima <tiago.jlima@yahoo.com.br>
2015-05-27 12:19:53 -0300

    Criação de Menu Lateral Esquerdo

--- public/js/controllers/chatController.js
+++ public/js/controllers/chatController.js
@@ -2,26 +2,41 @@ angular.module('chat').controller('chatController', function($scope){
 
 	var cores = ['alert alert-info', 'alert alert-success', 'alert alert-warning', 'alert alert-danger'],
 		coresIncrement = 0;
+		socket = io.connect(),
+		$messageBox = $('#message'),
+		$chat = $('#chat');
 
-	$(function(){
-		var socket = io.connect();
-		var $messageForm = $('#send-message');
-		var $messageBox = $('#message');
-		var $chat = $('#chat');
+	$scope.sendMessage = function(){
+		coresIncrement++;
+		if(coresIncrement == 3){
+			coresIncrement = 0;
+		}
+		socket.emit('send message', $messageBox.val());
+		$messageBox.val('');
+	};
 
-		$messageForm.submit(function(e){
-			e.preventDefault();
-			coresIncrement++;
-			if(coresIncrement == 3){
-				coresIncrement = 0;
-			}
-			socket.emit('send message', $messageBox.val());
-			$messageBox.val('');
-		});
+	$scope.class = "closed";
 
-		socket.on('new message', function(data){
-			$chat.append('<div class="'+cores[coresIncrement]+'">'+data+'</div>');
-		});
+	$scope.openStatusPerfil = function(){
+		if ($scope.class === "closed"){
+            $scope.class = "open";
+        }else{
+            $scope.class = "closed";
+        }
+	};
+
+	$scope.classMenu = "openMenu";
+
+	$scope.closeMenu = function(){
+		if ($scope.classMenu === "closedMenu"){
+            $scope.classMenu = "openMenu";
+        }else{
+            $scope.classMenu = "closedMenu";
+        }
+	};
+
+	socket.on('new message', function(data){
+		$chat.append('<div class="'+cores[coresIncrement]+'">'+data+'</div>');
 	});
 	
 });
